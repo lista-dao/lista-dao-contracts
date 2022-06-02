ff62ae9e1068b283a6ea37d5a5e69ed919a591c3
Alexandr Martirosyan <alexnadr.martirosyan2000@gmail.com>
2022-06-02 22:04:23 +0400

    fix compiler version

--- contracts/oracle/PriceOracle.sol
+++ contracts/oracle/PriceOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
--- contracts/oracle/interfaces/IUniswapV2Pair.sol
+++ contracts/oracle/interfaces/IUniswapV2Pair.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
 interface IUniswapV2Pair {
   event Approval(address indexed owner, address indexed spender, uint256 value);
