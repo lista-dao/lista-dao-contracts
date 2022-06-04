ddbf2e929caca3f504cd03c1f9a887dccb8363e7
Alex CryptoVL <es.maxim.vl@gmail.com>
2022-06-04 17:02:08 +0300

    Added events for stop|start rewards. Fixed cerosRouter

--- contracts/ceros/CerosRouter.sol
+++ contracts/ceros/CerosRouter.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.6;
-
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import "./interfaces/IVault.sol";
 import "./interfaces/IDex.sol";
 import "./interfaces/ICerosRouter.sol";
@@ -21,24 +19,18 @@ ReentrancyGuardUpgradeable
     /**
      * Variables
      */
-
     IVault private _vault;
     IDex private _dex;
     IBinancePool private _pool; // default (BinancePool)
-
     // Tokens
     ICertToken private _certToken; // (default aBNBc)
     address private _wBnbAddress;
     IERC20 private _ceToken; // (default ceABNBc)
-
     mapping(address => uint256) private _profits;
-
     address private _provider;
-
     /**
      * Modifiers
      */
-
     modifier onlyProvider() {
         require(
             msg.sender == owner() || msg.sender == _provider,
@@ -46,7 +38,6 @@ ReentrancyGuardUpgradeable
         );
         _;
     }
-
     function initialize(
         address certToken,
         address wBnbToken,
@@ -71,11 +62,9 @@ ReentrancyGuardUpgradeable
         IERC20(certToken).approve(pool, type(uint256).max);
         IERC20(certToken).approve(vault, type(uint256).max);
     }
-
     /**
      * DEPOSIT
      */
-
     function deposit()
     external
     payable
@@ -119,7 +108,7 @@ ReentrancyGuardUpgradeable
         // let's check balance of CeRouter in aBNBc
         require(
             _certToken.balanceOf(address(this)) >= realAmount,
-            "insufficient amount of CerosRouter in CeToken"
+            "insufficient amount of CerosRouter in cert token"
         );
         // add profit
         _profits[msg.sender] += profit;
@@ -127,7 +116,6 @@ ReentrancyGuardUpgradeable
         emit Deposit(msg.sender, _wBnbAddress, realAmount - profit, profit);
         return value;
     }
-
     function depositABNBcFrom(address owner, uint256 amount)
     external
     override
@@ -140,7 +128,6 @@ ReentrancyGuardUpgradeable
         emit Deposit(msg.sender, address(_certToken), amount, 0);
         return value;
     }
-
     function depositABNBc(uint256 amount)
     external
     override
@@ -152,11 +139,9 @@ ReentrancyGuardUpgradeable
         emit Deposit(msg.sender, address(_certToken), amount, 0);
         return value;
     }
-
     /**
      * CLAIM
      */
-
     // claim yields in aBNBc
     function claim(address recipient)
     external
@@ -168,7 +153,6 @@ ReentrancyGuardUpgradeable
         emit Claim(recipient, address(_certToken), yields);
         return yields;
     }
-
     // claim profit in aBNBc
     function claimProfit(address recipient) external nonReentrant {
         uint256 profit = _profits[msg.sender];
@@ -182,11 +166,9 @@ ReentrancyGuardUpgradeable
         _profits[msg.sender] -= profit;
         emit Claim(recipient, address(_certToken), profit);
     }
-
     /**
      * WITHDRAWAL
      */
-
     // withdrawal in BNB via staking pool
     /// @param recipient address to receive withdrawan BNB
     /// @param amount in BNB to withdraw from vault
@@ -201,11 +183,10 @@ ReentrancyGuardUpgradeable
             "value must be greater than min unstake amount"
         );
         realAmount = _vault.withdrawFor(msg.sender, address(this), amount);
-        _pool.unstakeCerts(recipient, realAmount);
+        _pool.unstakeCertsFor(recipient, realAmount);
         emit Withdrawal(msg.sender, recipient, _wBnbAddress, amount);
         return realAmount;
     }
-
     // withdrawal aBNBc
     /// @param recipient address to receive withdrawan aBNBc
     /// @param amount in BNB
@@ -219,7 +200,6 @@ ReentrancyGuardUpgradeable
         emit Withdrawal(msg.sender, recipient, address(_certToken), realAmount);
         return realAmount;
     }
-
     function withdrawFor(address recipient, uint256 amount)
     external
     override
@@ -228,11 +208,10 @@ ReentrancyGuardUpgradeable
     returns (uint256 realAmount)
     {
         realAmount = _vault.withdrawFor(msg.sender, address(this), amount);
-        _pool.unstakeCerts(recipient, realAmount); // realAmount -> BNB
+        _pool.unstakeCertsFor(recipient, realAmount); // realAmount -> BNB
         emit Withdrawal(msg.sender, recipient, _wBnbAddress, realAmount);
         return realAmount;
     }
-
     // withdrawal in BNB via DEX
     function withdrawWithSlippage(
         address recipient,
@@ -253,11 +232,9 @@ ReentrancyGuardUpgradeable
         emit Withdrawal(msg.sender, recipient, _wBnbAddress, amounts[1]);
         return amounts[1];
     }
-
     function getProfitFor(address account) external view returns (uint256) {
         return _profits[account];
     }
-
     function getPendingWithdrawalOf(address account)
     external
     view
@@ -265,7 +242,6 @@ ReentrancyGuardUpgradeable
     {
         return _pool.pendingUnstakesOf(account);
     }
-
     function changeVault(address vault) external onlyOwner {
         // update allowances
         _certToken.approve(address(_vault), 0);
@@ -273,7 +249,6 @@ ReentrancyGuardUpgradeable
         _certToken.approve(address(_vault), type(uint256).max);
         emit ChangeVault(vault);
     }
-
     function changeDex(address dex) external onlyOwner {
         IERC20(_wBnbAddress).approve(address(_dex), 0);
         _certToken.approve(address(_dex), 0);
@@ -283,7 +258,6 @@ ReentrancyGuardUpgradeable
         _certToken.approve(address(_dex), type(uint256).max);
         emit ChangeDex(dex);
     }
-
     function changePool(address pool) external onlyOwner {
         // update allowances
         _certToken.approve(address(_pool), 0);
@@ -291,7 +265,6 @@ ReentrancyGuardUpgradeable
         _certToken.approve(address(_pool), type(uint256).max);
         emit ChangePool(pool);
     }
-
     function changeProvider(address provider) external onlyOwner {
         _provider = provider;
         emit ChangeProvider(provider);
