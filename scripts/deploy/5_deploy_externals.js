81d02fa1883e4dd2f873841ad15f4f17a4eb13a8
Niocelbatsnu <kosmossphere@gmail.com>
2022-08-24 14:38:57 +0300

    Added operator to jar script

--- scripts/deploy/5_deploy_externals.js
+++ scripts/deploy/5_deploy_externals.js
@@ -6,7 +6,8 @@ const {ether} = require("@openzeppelin/test-helpers");
 async function main() {
 
   // Constant Addresses
-  let hay = "",
+  let hay = "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
+      operator = "0x57F9672bA603251C9C03B36cabdBBcA7Ca8Cfcf4",
       exitDelay = "1209600", // 14 Days
       spread = "1209600", // 14 Days
       flashLoanDelay = "5"; // 5 Seconds
@@ -25,6 +26,9 @@ async function main() {
   let jarImplementation = await upgrades.erc1967.getImplementationAddress(jar.address);
   console.log("Deployed: jar        : " + jar.address);
   console.log("Imp                  : " + jarImplementation);
+
+  // Jar Init
+  await jar.addOperator(operator);
 }
 
 main()
