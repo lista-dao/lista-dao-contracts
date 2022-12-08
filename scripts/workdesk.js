fe592a1a376553abd2c755bb0e947bcb5d606186
Niocelbatsnu <kosmossphere@gmail.com>
2022-12-08 11:55:30 +0500

    Added blacklist feature

--- scripts/workdesk.js
+++ scripts/workdesk.js
@@ -6,38 +6,19 @@ async function main() {
 
   let [deployer] = await ethers.getSigners();
 
-  this.Dog = await hre.ethers.getContractFactory("Dog");
-  this.AuctionProxy = await hre.ethers.getContractFactory("AuctionProxy");
-  const auctionProxy = await this.AuctionProxy.deploy();
-  await auctionProxy.deployed();
-  this.Interaction = await hre.ethers.getContractFactory("Interaction", {
-    unsafeAllow: ["external-library-linking"],
-    libraries: {
-      AuctionProxy: auctionProxy.address,
-    },
-  });
+  this.Hay = await hre.ethers.getContractFactory("Hay");
+  hay = await this.Hay.deploy(); await hay.deployed();
 
-  const interaction = await this.Interaction.deploy();
-  await interaction.deployed();
-  const dog = await this.Dog.deploy();
-  await dog.deployed();
-
-  console.log("Interaction    : " + interaction.address);
-  console.log("AucctionProxy  : " + auctionProxy.address);
-  console.log("Dog            : " + dog.address);
+  console.log("Hay            : " + hay.address);
 
   const addresses = {
-    interaction: interaction.address,
-    auctionProxy: auctionProxy.address,
-    dog: dog.address
+    hayimp: hay.address,
   };
 
   const jsonAddresses = JSON.stringify(addresses);
   fs.writeFileSync(`../${network.name}contractAddresses.json`, jsonAddresses);
 
-  await hre.run("verify:verify", {address: dog.address});
-  await hre.run("verify:verify", {address: interaction.address});
-  await hre.run("verify:verify", {address: auctionProxy.address});
+  await hre.run("verify:verify", {address: hay.address});
  }
 
 main()
