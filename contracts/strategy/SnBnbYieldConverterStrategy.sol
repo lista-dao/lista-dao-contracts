dbbb5e196161ea85c4941585b2aab70c15d8f1d3
xuegongjian0 <xuegongjian0@gmail.com>
2023-05-10 01:36:09 +0800

    add test script

--- contracts/strategy/SnBnbYieldConverterStrategy.sol
+++ contracts/strategy/SnBnbYieldConverterStrategy.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 import "../masterVault/interfaces/IMasterVault.sol";
-import "../synclub/interfaces/ISnBnbStakeManager.sol";
+import "../snbnb/interfaces/ISnBnbStakeManager.sol";
 import "./BaseStrategy.sol";
 
 contract SnBnbYieldConverterStrategy is BaseStrategy {
