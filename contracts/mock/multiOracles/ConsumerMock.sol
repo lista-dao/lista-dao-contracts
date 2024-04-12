9a6e79a6e0d587e66198018d411c33f7a4020953
Lawson Cheng <lawson.c@binance.com>
2024-04-13 00:31:10 +0800

    Added Mock Contracts for local testing

---
+++ contracts/mock/multiOracles/ConsumerMock.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.10;
+
+import "./interfaces/OracleInterface.sol";
+
+contract ConsumerMock {
+
+  OracleInterfaceMock private oracle;
+
+  uint256 public price;
+
+  constructor (address _oracle) {
+    oracle = OracleInterfaceMock(_oracle);
+  }
+
+  function getPrice(address asset) external {
+    price = oracle.getPrice(asset);
+  }
+
+}
