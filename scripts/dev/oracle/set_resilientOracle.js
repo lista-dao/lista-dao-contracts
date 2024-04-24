f6dc0e8e736ee20b58e8e58d2e5f3ee38ecdd97f
Lawson Cheng <lawson.c@binance.com>
2024-04-24 23:11:49 +0800

    ResilientOracle supports update time's delta checking

--- scripts/dev/oracle/set_resilientOracle.js
+++ scripts/dev/oracle/set_resilientOracle.js
@@ -5,13 +5,14 @@ async function main() {
   const ResilientOracle = await ethers.getContractFactory("ResilientOracle");
   const resilientOracle = await ResilientOracle.attach('0x2D9f861Fb030Fa2Bf9Ac64EBD11dF7f337bA7582')
   const tx = await resilientOracle.setTokenConfig([
-    '0xae13d989dac2f0debff460ac112a837c89baa7cd', // ResilientOracle
+    '0xae13d989dac2f0debff460ac112a837c89baa7cd', // asset token
     [
       '0x1A26d803C2e796601794f8C5609549643832702C',
       '0x806B5D50b4bf790853a3595BEC0767d1a002aD82',
       '0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526'
     ],
-    [true, true, true]
+    [true, true, true],
+    300
   ]);
   await tx.wait();
   console.log('done')
