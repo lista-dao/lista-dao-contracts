4bd2a94ae26747ece1747a792730d27429003a51
barry.c <barry.c@lista.org>
2024-05-05 22:59:17 +0800

    add mock price feed for ezeth/eth and update the ezeth oracle logic

---
+++ contracts/oracle/EzethOracle.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract EzethOracle is Initializable,AccessControl {
+
+    //AggregatorV3Interface internal ethUsdPriceFeed;
+    AggregatorV3Interface internal priceFeed;
+    AggregatorV3Interface internal ezethEthPriceFeed;
+
+
+    function initialize(address aggregatorAddress) external initializer {
+        priceFeed = AggregatorV3Interface(aggregatorAddress);
+    }
+
+
+    bytes32 public constant UPDATER_ROLE = keccak256("UPDATER_ROLE");
+
+/*    function initialize(address ethUsdAddr,address ezethEthAddr) external initializer {
+        ethUsdPriceFeed = AggregatorV3Interface(ethUsdAddr);
+        ezethEthPriceFeed = AggregatorV3Interface(ezethEthAddr);
+    }*/
+
+/*    function initialize(address aggregatorAddress1) external initializer {
+        priceFeed = AggregatorV3Interface(aggregatorAddress1);
+        _setupRole(UPDATER_ROLE, msg.sender);
+    }*/
+
+    function updateAddress(address ethUsdAddr,address ezethEthAddr) external {
+        require(hasRole(UPDATER_ROLE, msg.sender), "Caller is not an updater");
+        priceFeed = AggregatorV3Interface(ethUsdAddr);
+        ezethEthPriceFeed = AggregatorV3Interface(ethUsdAddr);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function peek() public view returns (bytes32, bool) {
+        (
+            /*uint80 roundID*/,
+            int price1,
+            /*uint startedAt*/,
+            uint timeStamp1,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+
+        require(block.timestamp - timeStamp1 < 300, "EthUsdOracle/timestamp-too-old");
+
+        (
+        /*uint80 roundID*/,
+            int price2,
+        /*uint startedAt*/,
+            uint timeStamp2,
+        /*uint80 answeredInRound*/
+        ) = ezethEthPriceFeed.latestRoundData();
+
+        require(block.timestamp - timeStamp2 < (13 * 3600), "EzethEthOracle/timestamp-too-old");
+
+        if (price1 <= 0 || price2 <= 0) {
+            return (0, false);
+        }
+
+
+        return (bytes32(uint(price1) * uint(price2) * (10**2)), true);
+    }
+}
---
+++ scripts/dev/deploy_ezethoracle_upgrade.js
@@ -0,0 +1,65 @@
+const {ethers, upgrades} = require('hardhat');
+
+async function main() {
+    console.log('Running deploy script');
+
+    let collateral = ethers.encodeBytes32String('EZETH');
+    //ETHUSD
+    let ethUsdPriceFeed = '0xc0e60De0CB09a432104C823D3150dDEEA90E8f7d'
+
+    //EzethEth
+    let ezEthEthPriceFeed = '0x77D231e51614C84e15CCC38E2a52BFab49D6853C'
+    let SPOT = '0x15493D9141481505f7CA3e591Cea2cBB03637B1d'
+
+    let proxyAddress = '0xeCf92977F937eAECf9F2124c4E3361d248A2988C';
+
+
+    const Oracle = await ethers.getContractFactory("EzethOracle");
+    const newImplAddress = await upgrades.prepareUpgrade(proxyAddress, Oracle);
+
+    console.log("新的合约实现已经部署在地址:", newImplAddress);
+
+
+
+    // 3. 更新代理使用新的合约实现
+    const upgradedOracle = await upgrades.upgradeProxy(proxyAddress, Oracle);
+    console.log("代理现在正在使用新的合约实现:", upgradedOracle.address);
+
+
+
+
+
+/*
+    this.Oracle = await hre.ethers.getContractFactory('EzethOracle')
+
+    const oracle = await upgrades.deployProxy(this.Oracle, [ethUsdPriceFeed])
+    await oracle.waitForDeployment()
+    let oracleImplementation = await upgrades.erc1967.getImplementationAddress(oracle.target)
+    console.log('Deployed: oracle     : ' + oracle.target)
+    console.log('Imp                  : ' + oracleImplementation)*/
+
+   // let oracleAddr = oracle.target;
+
+/*
+    this.Spot = await hre.ethers.getContractFactory("Spotter");
+
+    const spot = await this.Spot.attach(SPOT);
+    await spot["file(bytes32,bytes32,address)"](collateral, ethers.encodeBytes32String("pip"), oracle.target);
+    //await spot.poke(collateral);
+    console.log('set spot');
+*/
+
+
+    //await hre.run('verify:verify', {address: oracle.target})
+    await hre.run('verify:verify', {address: newImplAddress, contract: 'contracts/oracle/EzethOracle.sol:EzethOracle'})
+
+    console.log('Finished');
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
