2d87ed8f7773b09f667aaaab6bd928857f571a58
barry.c <barry.c@lista.org>
2024-05-08 10:13:11 +0800

    update ezeth oracle price feed

---
+++ contracts/oracle/EzethBinanceOracle.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract EzethOracle is Initializable {
+
+    AggregatorV3Interface internal priceFeed;
+
+    function initialize(address aggregatorAddress) external initializer {
+        priceFeed = AggregatorV3Interface(aggregatorAddress);
+    }
+
+    /**
+      * Returns the latest price
+      */
+    function peek() public view returns (bytes32, bool) {
+        (
+        /*uint80 roundID*/,
+            int price1,
+        /*uint startedAt*/,
+            uint timeStamp1,
+        /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+
+        require(block.timestamp - timeStamp1 < 300, "EthUsdOracle/timestamp-too-old");
+
+        return (bytes32(uint(price1) * (10**10)), true);
+    }
+}
