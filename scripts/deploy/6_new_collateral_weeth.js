8954a07eeebda22357237e4adf17dc176a1a907d
barry.c <barry.c@lista.org>
2024-05-14 18:50:25 +0800

    weeth

---
+++ scripts/deploy/6_new_collateral_weeth.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const path = require('path')
+const {ethers, upgrades} = require('hardhat')
+const {addCollateral} = require('../utils/add_collateral_weeth')
+
+// Global Variables
+let rad = '000000000000000000000000000000000000000000000' // 45 Decimals
+
+async function main() {
+
+  [deployer] = await ethers.getSigners()
+  let NEW_OWNER = '0xAca0ed4651ddA1F43f00363643CFa5EBF8774b37'
+
+  // Fetch factories
+  this.GemJoin = await hre.ethers.getContractFactory('GemJoin')
+  this.Clipper = await hre.ethers.getContractFactory('Clipper')
+  this.Oracle = await hre.ethers.getContractFactory('WeethOracle')
+
+  const symbol = 'WEETH_1'
+  let tokenAddress = '0x2416092f143378750bb29b79ed961ab195cceea5'
+  //todo replace this pricefeed address to real before going online
+  let priceFeed = '0x763c59a3D23936CD7B73571112744f2cFc2537F8'
+
+  if (hre.network.name === 'bsc_testnet') {
+    this.Oracle = await hre.ethers.getContractFactory('WeethOracleDev')
+    NEW_OWNER = deployer.address
+    console.log('Deploying on BSC Testnet', hre.network.name, 'Network', deployer.address)
+    // deploy token
+    const ERC20UpgradeableMock = await hre.ethers.getContractFactory('ERC20UpgradeableMock')
+    const tokenMock = await upgrades.deployProxy(ERC20UpgradeableMock, [symbol, symbol])
+    await tokenMock.waitForDeployment()
+    const tokenMockImplementation = await upgrades.erc1967.getImplementationAddress(tokenMock.target)
+    console.log('Deployed: clipCE     : ' + tokenMock.target)
+    console.log('Imp                  : ' + tokenMockImplementation)
+    tokenAddress = await tokenMock.target
+    //await hre.run('verify:verify', {address: tokenMock.target})
+    //await hre.run('verify:verify', {address: tokenMockImplementation, contract: 'contracts/mock/ERC20UpgradeableMock.sol:ERC20UpgradeableMock'})
+    // mint 10000000 tokens to deployer
+    await tokenMock.mint(deployer.address, ethers.parseEther('10000000'))
+
+    // 这是mock的price feed
+    priceFeed = '0x77D231e51614C84e15CCC38E2a52BFab49D6853C'
+  }
+
+  // add collateral
+  await addCollateral({
+    symbol,
+    tokenAddress,
+    priceFeed,
+    owner: NEW_OWNER,
+    clipperBuf: '1100000000000000000000000000',
+    clipperTail: '10800',
+    clipperCusp: '600000000000000000000000000',
+    clipperChip: '0',
+    clipperTip: '5' + rad,
+    clipperStopped: '0'
+  })
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
---
+++ scripts/dev/add_collateral_weeth.js
@@ -0,0 +1,107 @@
+const {ethers, upgrades} = require('hardhat')
+const hre = require('hardhat')
+// Global Variables
+let ray = '000000000000000000000000000', // 27 Decimals
+  rad = '000000000000000000000000000000000000000000000' // 45 Decimals
+
+async function main() {
+
+  [deployer] = await ethers.getSigners()
+
+  const {symbol, tokenAddress, ilk, gemJoin, clipper, oracle} = {
+    "symbol": "WEETH_1",
+    "tokenAddress": "0x4F6e6259c46f4079cF0CEcFCa7c30FdCDa35E8aF",
+    "ilk": "0x57454554485f3100000000000000000000000000000000000000000000000000",
+    "gemJoin": "0x50cb4d1a1854c0230F1Ec104e797F3C9cc42b83b",
+    "gemJoinImplementation": "0xB64BDeBdC7572D48fb29fDB1352080abD9bc2fc8",
+    "clipper": "0xF1Ed16c72493354885BE65F583A3Dc47BAfAe0e2",
+    "clipperImplementation": "0x21f8Ff25c0cE07521dF5c10c2E04f13F86325988",
+    "oracle": "0x8e8dA7b46F2596D53c94f2F0eB4ef98e5ce38349",
+    "oracleImplementation": "0xbedF0ABD16d87C4ed0DDF1Bc40dfF1926eb4dd6a",
+    "weethEthpriceFeed": "0xFe6BdDA126f5EE3f0B73D5A9Cdf7943d46ef6f28",
+    "ethUsdpriceFeed": "0x635780E5D02Ab29d7aE14d266936A38d3D5B0CC5"
+  }
+
+  // core parameters
+  const mat = '2000000000000000000000000000' // Liquidation Ratio
+  const line = '1000000' + rad // Debt Ceiling
+  const dust = '15' + rad // Debt Floor
+  const hole = '5000000' + rad // Liquidation
+  const chop = '1100000000000000000' // Liquidation
+
+
+  let VAT = '0x33A34eAB3ee892D40420507B820347b1cA2201c4'
+  let DOG = '0xd57E7b53a1572d27A04d9c1De2c4D423f1926d0B'
+  let SPOT = '0x49bc2c4E5B035341b7d92Da4e6B267F7426F3038'
+  let INTERACTION = '0xB68443Ee3e828baD1526b3e0Bdf2Dfc6b1975ec4'
+  let AUCTION_PROXY
+
+  if (hre.network.name === 'bsc_testnet') {
+    VAT = '0xC9eeBDB18bD05dCF981F340b838E8CdD946D60ad'
+    DOG = '0x77e4FcEbCDd30447f6e2E486B00a552A6493da0F'
+    SPOT = '0x15493D9141481505f7CA3e591Cea2cBB03637B1d'
+    INTERACTION = '0xb7A5999AEaE17C37d07ac4b34e56757c96387c84'
+    if (!AUCTION_PROXY) {
+      // deploy AuctionProxy
+      const AuctionProxy = await hre.ethers.getContractFactory('AuctionProxy')
+      const auctionProxy = await AuctionProxy.deploy()
+      await auctionProxy.waitForDeployment()
+      AUCTION_PROXY = await auctionProxy.getAddress()
+      console.log('AuctionProxy deployed to:', AUCTION_PROXY)
+    }
+  }
+
+
+  console.log('symbol: ' + symbol)
+  console.log('tokenAddress: ' + tokenAddress)
+  console.log('ilk: ' + ilk)
+
+  // configure the collateral
+  console.log('interaction...')
+  this.Interaction = await hre.ethers.getContractFactory('Interaction', {
+    unsafeAllow: ['external-library-linking'],
+    libraries: {
+      AuctionProxy: AUCTION_PROXY
+    },
+  })
+  const interaction = this.Interaction.attach(INTERACTION)
+  //await interaction.removeCollateralType(tokenAddress)
+
+  await interaction.setCollateralType(tokenAddress, gemJoin, ilk, clipper, mat)
+
+  console.log('vat...')
+  this.Vat = await hre.ethers.getContractFactory('Vat')
+  const vat = this.Vat.attach(VAT)
+  await vat.rely(gemJoin)
+  await vat.rely(clipper)
+  await vat['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('dust'), dust)
+  await vat['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('line'), line)
+
+  console.log('spot...')
+  this.Spot = await hre.ethers.getContractFactory('Spotter')
+  const spot = this.Spot.attach(SPOT)
+  await spot['file(bytes32,bytes32,address)'](ilk, ethers.encodeBytes32String('pip'), oracle)
+
+  console.log('Dog...')
+  this.Dog = await hre.ethers.getContractFactory('Dog')
+  const dog = this.Dog.attach(DOG)
+  await dog.rely(clipper)
+  await dog['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('hole'), hole)
+  await dog['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('chop'), chop) // 10%
+  await dog['file(bytes32,bytes32,address)'](ilk, ethers.encodeBytes32String('clip'), clipper)
+
+  await interaction.setCollateralDuty(tokenAddress, '1000000004431822000000000000'); //apr 15%
+  console.log("set duty...");
+
+  await interaction.poke(tokenAddress, { gasLimit: 1000000 })
+  await interaction.drip(tokenAddress, { gasLimit: 1000000 })
+
+  console.log('Finished')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
