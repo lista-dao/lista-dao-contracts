760f17d5f3530baa7bab353c36c18695c6e044b0
Lawson Cheng <lawson.c@binance.com>
2024-05-14 23:45:27 +0800

    Updated API3Oracle deployment script

--- scripts/deploy/deploy_api3_agent.js
+++ scripts/deploy/deploy_api3_agent.js
@@ -5,18 +5,18 @@ const API3ProxyAddress = '';
 
 async function main() {
   const API3Oracle = await ethers.getContractFactory("API3Oracle");
-  const api3Oracle = await upgrades.deployProxy(API3Oracle, [API3ProxyAddress], { initializer: "initialize" });
+  const api3Oracle = await API3Oracle.deploy();
   await api3Oracle.waitForDeployment();
-
-  let api3OracleImplementation = await upgrades.erc1967.getImplementationAddress(api3Oracle.target);
-  console.log("Deployed: API3Oracle    : " + api3Oracle.target);
-  console.log("Imp                     : " + api3OracleImplementation);
-
+  const api3OracleAddress = await api3Oracle.getAddress();
+  console.log('contract deployed at: ', api3OracleAddress);
   console.log('---------- verifying contract ----------')
   await hre.run("verify:verify", {
-    address: api3Oracle.target,
-    constructorArguments: [API3ProxyAddress]
+    address: api3OracleAddress
   });
+  // set proxy address
+  const tx = await api3Oracle.initialize(API3ProxyAddress);
+  await tx.wait(3);
+  console.log('proxy is set');
 }
 
 main()
