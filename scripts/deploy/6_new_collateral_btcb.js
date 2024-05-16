0f07ee6b3eb1146eb97c04e0bf29d179de2378ae
yq <qing.yang@jupitercim.com>
2024-05-16 21:55:44 +0800

    perf: optimize add collateral script

--- scripts/deploy/6_new_collateral_btcb.js
+++ scripts/deploy/6_new_collateral_btcb.js
@@ -10,6 +10,7 @@ async function main() {
 
   [deployer] = await ethers.getSigners()
   let NEW_OWNER = '0xAca0ed4651ddA1F43f00363643CFa5EBF8774b37'
+  let PROXY_ADMIN_OWNER = '0x08aE09467ff962aF105c23775B9Bc8EAa175D27F'
 
   // Fetch factories
   this.GemJoin = await hre.ethers.getContractFactory('GemJoin')
@@ -23,14 +24,15 @@ async function main() {
   let oracleInitializer = 'initialize';
 
   if (hre.network.name === 'bsc_testnet') {
-    NEW_OWNER = deployer.address
+    NEW_OWNER = process.env.OWNER || deployer.address
+    PROXY_ADMIN_OWNER = process.env.PROXY_ADMIN_OWNER || deployer.address
     console.log('Deploying on BSC Testnet', hre.network.name, 'Network', deployer.address)
     // deploy token
     const ERC20UpgradeableMock = await hre.ethers.getContractFactory('ERC20UpgradeableMock')
     const tokenMock = await upgrades.deployProxy(ERC20UpgradeableMock, [symbol, symbol])
     await tokenMock.waitForDeployment()
     const tokenMockImplementation = await upgrades.erc1967.getImplementationAddress(tokenMock.target)
-    console.log('Deployed: clipCE     : ' + tokenMock.target)
+    console.log('Deployed: tokenMock     : ' + tokenMock.target)
     console.log('Imp                  : ' + tokenMockImplementation)
     tokenAddress = await tokenMock.target
     await hre.run('verify:verify', {address: tokenMock.target})
@@ -48,6 +50,7 @@ async function main() {
     oracleInitializeArgs,
     oracleInitializer,
     owner: NEW_OWNER,
+    proxyAdminOwner: PROXY_ADMIN_OWNER,
     clipperBuf: '1100000000000000000000000000',
     clipperTail: '10800',
     clipperCusp: '600000000000000000000000000',
