e078ededeedeac4e78a18036b0a815b85927f25f
Lawson Cheng <lawson.c@binance.com>
2024-05-21 23:54:24 +0800

    Updated resilientOracle configuration scripts

--- scripts/prod/oracle/set_boundValidator.js
+++ scripts/prod/oracle/set_boundValidator.js
@@ -11,12 +11,20 @@ async function main() {
   for (let assetName in config.assets) {
     const asset = config.assets[assetName];
     console.log(`[BoundValidator] setting ${assetName}...`);
-    const tx = await boundValidator.setValidateConfig([
-      asset.token,
-      asset.upperBoundRatio,
-      asset.lowerBoundRatio
-    ]);
-    await tx.wait(2);
+    // check is configured
+    const validateConfig = await boundValidator.validateConfigs(asset.token);
+    console.log(validateConfig.asset, asset.token);
+    // not configured
+    if ((validateConfig.asset || "").toLowerCase() !== asset.token.toLowerCase()) {
+      const tx = await boundValidator.setValidateConfig([
+        asset.token,
+        asset.upperBoundRatio,
+        asset.lowerBoundRatio
+      ]);
+      await tx.wait(2);
+    } else {
+      console.log("[BoundValidator] configured already")
+    }
   }
   console.log('[BoundValidator] config done')
 }
