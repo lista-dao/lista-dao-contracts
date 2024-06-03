8dadcb37bae70abaa137cbc2c2c8990025e81b0b
barry.c <barry.c@lista.org>
2024-06-03 17:49:30 +0800

    add new oracle

--- scripts/dev/deploy_mock_oracle.js
+++ scripts/dev/deploy_mock_oracle.js
@@ -3,14 +3,18 @@ const {ethers, upgrades} = require("hardhat");
 
 async function main() {
     console.log('Running deploy script');
-
-    const Oracle = await hre.ethers.getContractFactory("MockPriceFeed");
+    let [owner] = await ethers.getSigners();
+    const Oracle = await hre.ethers.getContractFactory("MultiOracleMock");
     const oracle = await Oracle.deploy();
     await oracle.waitForDeployment();
 
-    await hre.run("verify:verify", {address: oracle.target});
+    //await hre.run("verify:verify", {address: oracle.target});
 
     console.log('oracle deploy at', oracle.target);
+
+    let hasRole = await oracle.hasRole('0x73e573f9566d61418a34d5de3ff49360f9c51fec37f7486551670290f6285dab', owner);
+    console.log('DEPLOYER: %s has ADMIN_ROLE: %s', owner.address, hasRole);
+    console.log('Finished');
 
 }
 
