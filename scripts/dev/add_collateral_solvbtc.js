6403cb656aa3c95bc27ec635f8b79324e9c5142c
barry.c <barry.c@lista.org>
2024-06-04 00:55:01 +0800

    add collaterals

---
+++ scripts/dev/add_collateral_solvbtc.js
@@ -0,0 +1,109 @@
+const {ethers, upgrades} = require('hardhat')
+const hre = require('hardhat')
+// Global Variables
+let ray = '000000000000000000000000000', // 27 Decimals
+  rad = '000000000000000000000000000000000000000000000' // 45 Decimals
+
+async function main() {
+
+  [deployer] = await ethers.getSigners()
+
+  const {symbol, tokenAddress, ilk, gemJoin, clipper, oracle} = {
+    "symbol": "solvBTC",
+    "tokenAddress": "0xB1E63330f4718772CF939128d222389b30C70cF2",
+    "ilk": "0x736f6c7642544300000000000000000000000000000000000000000000000000",
+    "gemJoin": "0x7dd069c670912Df55Df40541aCf0102E28372b65",
+    "gemJoinImplementation": "0xB64BDeBdC7572D48fb29fDB1352080abD9bc2fc8",
+    "clipper": "0x7eb1E2eDCC1e504256846dBfcbe7498A078B887e",
+    "clipperImplementation": "0x21f8Ff25c0cE07521dF5c10c2E04f13F86325988",
+    "oracle": "0x4380f9E81a9456a4E5C5D194cE0b8F2164E2e51E",
+    "oracleImplementation": "0x2FED3a32D478f3eE124D4b9c597c473922EB3cBe",
+    "oracleInitializeArgs": [
+      ""
+    ],
+    "owner": "0x0C6f6b0C6f78950445133FADe7DECD64c0bDd093",
+    "proxyAdminOwner": "0x0C6f6b0C6f78950445133FADe7DECD64c0bDd093"
+  }
+
+  // core parameters
+  const mat = '2000000000000000000000000000' // Liquidation Ratio
+  const line = '500000' + rad // Debt Ceiling
+  const dust = '15' + rad // Debt Floor
+  const hole = '5000000' + rad // Liquidation
+  const chop = '1100000000000000000' // Liquidation
+
+
+  let VAT = '0x33A34eAB3ee892D40420507B820347b1cA2201c4'
+  let DOG = '0xd57E7b53a1572d27A04d9c1De2c4D423f1926d0B'
+  let SPOT = '0x49bc2c4E5B035341b7d92Da4e6B267F7426F3038'
+  let INTERACTION = '0xB68443Ee3e828baD1526b3e0Bdf2Dfc6b1975ec4'
+  let AUCTION_PROXY
+
+  if (hre.network.name === 'bsc_testnet') {
+    VAT = '0x382589e4dE7A061fcb9716c203983d8FE847AE0b'
+    DOG = '0x3d2165EDf3Cc07992f54d9310FB800C81BC641F7'
+    SPOT = '0xa2882B6AC7cBA1b8784BF5D72F38CF0E6416263e'
+    INTERACTION = '0x70C4880A3f022b32810a4E9B9F26218Ec026f279'
+    if (!AUCTION_PROXY) {
+      // deploy AuctionProxy
+      const AuctionProxy = await hre.ethers.getContractFactory('AuctionProxy')
+      const auctionProxy = await AuctionProxy.deploy()
+      await auctionProxy.waitForDeployment()
+      AUCTION_PROXY = await auctionProxy.getAddress()
+      console.log('AuctionProxy deployed to:', AUCTION_PROXY)
+    }
+  }
+
+
+  console.log('symbol: ' + symbol)
+  console.log('tokenAddress: ' + tokenAddress)
+  console.log('ilk: ' + ilk)
+
+  // configure the collateral
+  console.log('interaction...')
+  this.Interaction = await hre.ethers.getContractFactory('Interaction', {
+    unsafeAllow: ['external-library-linking'],
+    libraries: {
+      AuctionProxy: AUCTION_PROXY
+    },
+  })
+  const interaction = this.Interaction.attach(INTERACTION)
+  await interaction.setCollateralType(tokenAddress, gemJoin, ilk, clipper, mat)
+
+  console.log('vat...')
+  this.Vat = await hre.ethers.getContractFactory('Vat')
+  const vat = this.Vat.attach(VAT)
+  await vat.rely(gemJoin)
+  await vat.rely(clipper)
+  await vat['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('dust'), dust)
+  await vat['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('line'), line)
+
+  console.log('spot...')
+  this.Spot = await hre.ethers.getContractFactory('Spotter')
+  const spot = this.Spot.attach(SPOT)
+  await spot['file(bytes32,bytes32,address)'](ilk, ethers.encodeBytes32String('pip'), oracle)
+
+  console.log('Dog...')
+  this.Dog = await hre.ethers.getContractFactory('Dog')
+  const dog = this.Dog.attach(DOG)
+  await dog.rely(clipper)
+  await dog['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('hole'), hole)
+  await dog['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('chop'), chop) // 10%
+  await dog['file(bytes32,bytes32,address)'](ilk, ethers.encodeBytes32String('clip'), clipper)
+
+
+  await interaction.setCollateralDuty(tokenAddress, '1000000003734875566854894262'); //apr 15%
+  console.log("set duty...");
+
+  await interaction.poke(tokenAddress, { gasLimit: 1000000 })
+  await interaction.drip(tokenAddress, { gasLimit: 1000000 })
+
+  console.log('Finished')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
