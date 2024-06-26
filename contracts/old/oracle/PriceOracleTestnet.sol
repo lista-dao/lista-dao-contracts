c66e684deff8790432ec4cedad02b225860f2f54
barry.c <barry.c@lista.org>
2024-06-26 18:18:12 +0800

    refact no use code to old

--- contracts/old/oracle/PriceOracleTestnet.sol
+++ contracts/old/oracle/PriceOracleTestnet.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-import { IMovingWindowOracle } from "../interfaces/IMovingWindowOracle.sol";
+import { IMovingWindowOracle } from "../../interfaces/IMovingWindowOracle.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
