e354be80f7e3b87392bd10b1b1d652718321dc45
yq <qing.yang@jupitercim.com>
2024-06-26 18:32:58 +0800

    chore: move deprecated contracts of Bnbx to old folder

--- contracts/old/strategy/BnbxYieldConverterStrategy.sol
+++ contracts/old/strategy/BnbxYieldConverterStrategy.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 import "../../masterVault/interfaces/IMasterVault.sol";
-import "../../bnbx/interfaces/IStakeManager.sol";
+import "./bnbx/interfaces/IStakeManager.sol";
 import "../../strategy/BaseStrategy.sol";
 
 contract BnbxYieldConverterStrategy is BaseStrategy {
