3dfcda84eaf9bbbd42e53d69db7322183e8b0eac
Lawson Cheng <lawson.c@binance.com>
2024-07-23 17:15:46 +0800

    Added implementation scripts of hETH and hBNB

---
+++ scripts/upgrades/ceros/deploy_clisETH_impl.js
@@ -0,0 +1,26 @@
+const hre = require('hardhat');
+const {ethers} = hre;
+
+const contractName = 'clisETH'
+
+async function main() {
+  console.log('Running deploy script...')
+  const contractFactory = await ethers.getContractFactory(contractName);
+  const contractImpl = await contractFactory.deploy();
+  await contractImpl.deploymentTransaction().wait(3);
+  console.log("Contract deployed.");
+  const address = await contractImpl.getAddress();
+  console.log(`${contractName} Implementation: `, address)
+  // verify
+  await hre.run("verify:verify", {
+    address
+  });
+  console.log("---- Done ----");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
