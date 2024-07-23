f86cdba6a5013a9c2e456d9d12a9ed520354debb
Lawson Cheng <lawson.c@binance.com>
2024-07-23 17:15:24 +0800

    Added change name and symbol methods to hETH and hBNB contract

--- contracts/ceros/hBNB.sol
+++ contracts/ceros/hBNB.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 import "./NonTransferableERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract hBNB is OwnableUpgradeable, NonTransferableERC20 {
+contract clisBNB is OwnableUpgradeable, NonTransferableERC20 {
     /**
      * Variables
      */
@@ -29,6 +29,16 @@ contract hBNB is OwnableUpgradeable, NonTransferableERC20 {
     function initialize() external initializer {
         __Ownable_init();
         __ERC20_init_unchained("Helio BNB", "hBNB");
+    }
+
+    function setName(string memory newName) external onlyOwner {
+        require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked(newName)), "Name: new name cannot be the same as the current name");
+        _name = newName;
+    }
+
+    function setSymbol(string memory newSymbol) external onlyOwner {
+        require(keccak256(abi.encodePacked(_symbol)) != keccak256(abi.encodePacked(newSymbol)), "Symbol: new symbol cannot be the same as the current symbol");
+        _symbol = newSymbol;
     }
 
     function burn(address account, uint256 amount) external onlyMinter {
