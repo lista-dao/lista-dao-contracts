1931c5810d9ef334a2f240cb92d8195b44a60721
Lawson Cheng <lawson.c@binance.com>
2024-08-20 00:02:07 +0800

    Edited mock contract solidity ver.

--- contracts/mock/multiOracles/MockSourceOracle.sol
+++ contracts/mock/multiOracles/MockSourceOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import "./interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
