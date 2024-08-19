6bf37d61d506713406c20f11e79d473cb9c8f856
Lawson Cheng <lawson.c@binance.com>
2024-08-19 15:07:38 +0800

    Added general proxy deployment script

---
+++ scripts/deploy/deploy_new_proxy_contract.js
@@ -0,0 +1,29 @@
+const hre = require("hardhat");
+const {ethers, upgrades} = hre;
+
+// -------------------------------------------- //
+const contractName = '';
+const initializerArgs = [];
+// -------------------------------------------- //
+
+async function main() {
+  // Deploy resilientOracle
+  const Contract = await ethers.getContractFactory(contractName);
+  const proxyContract = await upgrades.deployProxy(Contract, initializerArgs, { initializer: "initialize" });
+  await proxyContract.waitForDeployment();
+
+  let proxyContractImpl = await upgrades.erc1967.getImplementationAddress(proxyContract.target);
+  console.log(`Deployed: ${contractName}: ` + proxyContract.target);
+  console.log("Imp                      : " + proxyContractImpl);
+
+  // verify contracts
+  console.log(`---------- verifying ${contractName} ----------`)
+  await hre.run("verify:verify", { address: proxyContractImpl.target });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
