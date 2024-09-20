79ba87ea0e3e1cc6df91950e9ef0096fe66c971e
Razorback <raz.w@binance.com>
2024-09-20 14:59:49 +0800

    feat: adjust time for auction reset

--- scripts/dev/update_bnb_mcr.ts
+++ scripts/dev/update_bnb_mcr.ts
@@ -6,10 +6,12 @@ const BNB_CLIPPER = "0x14DD6c86d24A72b42648D9a642318375A26ed108";
 const BNB_MAT = "1200000000000000000000000000"; // 120% mcr
 const BNB_ILK =
   "0x636541424e426300000000000000000000000000000000000000000000000000";
+const TAU = "3600"; // 1 hour
+const BNB_TAIL = "1200"; // 20 minutes elapsed before reset
 
 //////////// Adjust BNB MCR ////////////
 /// 1. deploy LinearDecrease contract
-/// 2. set tau to 5 hours
+/// 2. set tau to 1 hours
 /// 3. set Clipper calc to new LinearDecrease
 /// 4. set Clipper mat to new MCR
 /// 5. verify LinearDecrease contract
@@ -20,14 +22,17 @@ async function main() {
 
   console.log("LinearDecrease deployed to:", abaci.target);
 
-  const tau = "18000"; // 5 hours
-  await abaci.file(ethers.encodeBytes32String("tau"), tau);
+  await abaci.file(ethers.encodeBytes32String("tau"), TAU);
 
   const Clipper = await ethers.getContractFactory("Clipper");
   const clipper = Clipper.attach(BNB_CLIPPER);
   await clipper["file(bytes32,address)"](
     ethers.encodeBytes32String("calc"),
     abaci.target
+  );
+  await clipper["file(bytes32,uint256)"](
+    ethers.encodeBytes32String("tail"),
+    BNB_TAIL
   );
 
   const Spotter = await ethers.getContractFactory("Spotter");
@@ -42,7 +47,8 @@ async function main() {
 
   console.log("tau set to: ", (await abaci.tau()).toString());
   console.log("Clipper calc set to: ", (await clipper.calc()).toString());
-  console.log("Clipper mat set to: ", newMat.toString());
+  console.log("Clipper tail set to: ", (await clipper.tail()).toString());
+  console.log("Spotter mat set to: ", newMat.toString());
 
   await run("verify:verify", {
     address: abaci.target,
