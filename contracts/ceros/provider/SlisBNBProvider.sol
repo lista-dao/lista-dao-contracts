c4e3933090dff57125b8359290341bd4520a0b2d
ted-w <ted.w@lista.org>
2024-11-25 12:14:21 +0800

    = remove unused imports

--- contracts/ceros/provider/SlisBNBProvider.sol
+++ contracts/ceros/provider/SlisBNBProvider.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
