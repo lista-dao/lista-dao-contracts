c6366153a43cc9eb1d10e494aacc53fb0c7b6db0
rick <jing.xin@lista.org>
2024-12-09 18:42:38 +0800

    add collateral deploy oracle

--- scripts/deploy/9_new_collateral_susdx.js
+++ scripts/deploy/9_new_collateral_susdx.js
@@ -4,6 +4,7 @@ const contractAddresses = require("./contract_address.json");
 const {transferProxyAdminOwner} = require("../upgrades/utils/upgrade_utils");
 const path = require("path");
 const fs = require("fs");
+const {addCollateral} = require('../utils/add_collateral')
 
 // Global Variables
 let rad = '000000000000000000000000000000000000000000000' // 45 Decimals
@@ -12,7 +13,10 @@ async function main() {
 
     const [deployer] = await ethers.getSigners()
     let NEW_OWNER = '0xAca0ed4651ddA1F43f00363643CFa5EBF8774b37'
-    let PROXY_ADMIN_OWNER = '0x08aE09467ff962aF105c23775B9Bc8EAa175D27F'
+    let PROXY_ADMIN_OWNER = '0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253'
+    let oracleName = 'sUsdxOracle';
+    let oracleInitializeArgs = ['0xf3afD82A4071f272F403dC176916141f44E6c750'];
+    let oracleInitializer = 'initialize';
 
     const symbol = 'sUSDX'
     let tokenAddress = '0x7788A3538C5fc7F9c7C8A74EAC4c898fC8d87d92' // sUSDX token address on BSC Mainnet
@@ -28,6 +32,9 @@ async function main() {
     await addCollateral({
         symbol,
         tokenAddress,
+        oracleName,
+        oracleInitializeArgs,
+        oracleInitializer,
         owner: NEW_OWNER,
         proxyAdminOwner: PROXY_ADMIN_OWNER,
         clipperBuf: '1100000000000000000000000000',
@@ -45,130 +52,3 @@ main()
         console.error(error)
         process.exit(1)
     })
-
-
-async function addCollateral(opts) {
-    const {
-        symbol,
-        tokenAddress,
-        owner,
-        proxyAdminOwner,
-        clipperBuf,
-        clipperTail,
-        clipperCusp,
-        clipperChip,
-        clipperTip,
-        clipperStopped,
-    } = opts;
-
-    const {
-        VAT,
-        DOG,
-        SPOT,
-        INTERACTION,
-        VOW,
-        ABACI,
-        JUG
-    } = (hre.network.name === 'bsc_testnet') ? contractAddresses["testnet"] : contractAddresses["mainnet"];
-
-
-    [deployer] = await ethers.getSigners();
-    let NEW_OWNER = owner || '0xAca0ed4651ddA1F43f00363643CFa5EBF8774b37'
-    let NEW_PROXY_ADMIN_OWNER = proxyAdminOwner || '0x08aE09467ff962aF105c23775B9Bc8EAa175D27F'
-
-    // Fetch factories
-    this.GemJoin = await hre.ethers.getContractFactory('GemJoin')
-    this.Clipper = await hre.ethers.getContractFactory('Clipper')
-
-    // Set addresses
-    const ILK = ethers.encodeBytes32String(symbol)
-
-    if (hre.network.name === 'bsc_testnet') {
-        NEW_OWNER = owner || deployer.address
-        NEW_PROXY_ADMIN_OWNER = proxyAdminOwner || deployer.address
-    }
-
-    // Deploy contracts
-    const gemJoin = await upgrades.deployProxy(this.GemJoin, [VAT, ILK, tokenAddress])
-    await gemJoin.waitForDeployment()
-    let gemJoinImplementation = await upgrades.erc1967.getImplementationAddress(gemJoin.target)
-    console.log('Deployed: gemJoin    : ' + gemJoin.target)
-    console.log('Imp                  : ' + gemJoinImplementation)
-    // transfer proxy admin ownership
-    if (deployer.address !== NEW_PROXY_ADMIN_OWNER) {
-        await transferProxyAdminOwner(gemJoin.target, NEW_PROXY_ADMIN_OWNER)
-        console.log('Proxy Admin Ownership Of gemJoin Transferred to: ' + NEW_PROXY_ADMIN_OWNER)
-    }
-
-    const clipper = await upgrades.deployProxy(this.Clipper, [VAT, SPOT, DOG, ILK])
-    await clipper.waitForDeployment()
-    let clipperImplementation = await upgrades.erc1967.getImplementationAddress(clipper.target)
-    console.log('Deployed: clipCE     : ' + clipper.target)
-    console.log('Imp                  : ' + clipperImplementation)
-    // transfer proxy admin ownership
-    if (deployer.address !== NEW_PROXY_ADMIN_OWNER) {
-        await transferProxyAdminOwner(clipper.target, NEW_PROXY_ADMIN_OWNER)
-        console.log('Proxy Admin Ownership Of clipCE Transferred to: ' + NEW_PROXY_ADMIN_OWNER)
-    }
-
-    // Initialize
-    await gemJoin.rely(INTERACTION)
-
-    await clipper.rely(DOG)
-    await clipper.rely(INTERACTION)
-    await clipper['file(bytes32,uint256)'](ethers.encodeBytes32String('buf'), clipperBuf) // 10%
-    await clipper['file(bytes32,uint256)'](ethers.encodeBytes32String('tail'), clipperTail) // 3h reset time
-    await clipper['file(bytes32,uint256)'](ethers.encodeBytes32String('cusp'), clipperCusp) // 60% reset ratio
-    await clipper['file(bytes32,uint256)'](ethers.encodeBytes32String('chip'), clipperChip) // 0.01% from vow incentive
-    await clipper['file(bytes32,uint256)'](ethers.encodeBytes32String('tip'), clipperTip) // 10$ flat fee incentive
-    await clipper['file(bytes32,uint256)'](ethers.encodeBytes32String('stopped'), clipperStopped)
-
-    await clipper['file(bytes32,address)'](ethers.encodeBytes32String('spotter'), SPOT)
-    await clipper['file(bytes32,address)'](ethers.encodeBytes32String('dog'), DOG)
-    await clipper['file(bytes32,address)'](ethers.encodeBytes32String('vow'), VOW)
-    await clipper['file(bytes32,address)'](ethers.encodeBytes32String('calc'), ABACI)
-
-    // Transfer Ownerships
-    await gemJoin.rely(NEW_OWNER)
-    await clipper.rely(NEW_OWNER)
-
-    if (deployer.address !== NEW_OWNER) {
-        await gemJoin.deny(deployer.address)
-        await clipper.deny(deployer.address)
-    }
-
-    console.log('token address: ' + tokenAddress)
-    console.log(ILK)
-
-    // Store deployed addresses
-    const addresses = {
-        symbol,
-        tokenAddress,
-        ilk: ILK,
-        gemJoin: gemJoin.target,
-        gemJoinImplementation: gemJoinImplementation,
-        clipper: clipper.target,
-        clipperImplementation: clipperImplementation,
-        owner: NEW_OWNER,
-        proxyAdminOwner: NEW_PROXY_ADMIN_OWNER,
-    }
-
-    const json_addresses = JSON.stringify(addresses)
-    console.log('json addresses: ', json_addresses)
-    const dir = path.join(__dirname, `../../addresses/new_collateral_${symbol}_${hre.network.name}.json`)
-    fs.writeFileSync(dir, json_addresses)
-    console.log('Addresses Recorded to: ' + dir)
-
-    // Verify
-    try {
-        await hre.run('verify:verify', {address: gemJoin.target})
-    } catch (err) {
-        console.log(err.message)
-    }
-    try {
-        await hre.run('verify:verify', {address: clipper.target})
-    } catch (err) {
-        console.log(err.message)
-    }
-
-}
--- scripts/utils/add_collateral.js
+++ scripts/utils/add_collateral.js
@@ -11,7 +11,7 @@ module.exports.addCollateral = async function (opts) {
   const {
     symbol,
     tokenAddress,
-    oracleName = 'BtcOracle',
+    oracleName = 'sUsdxOracle',
     oracleInitializeArgs = [],
     oracleInitializer = 'initialize',
     owner,
@@ -37,7 +37,7 @@ module.exports.addCollateral = async function (opts) {
 
   [deployer] = await ethers.getSigners();
   let NEW_OWNER = owner || '0xAca0ed4651ddA1F43f00363643CFa5EBF8774b37'
-  let NEW_PROXY_ADMIN_OWNER = proxyAdminOwner || '0x08aE09467ff962aF105c23775B9Bc8EAa175D27F'
+  let NEW_PROXY_ADMIN_OWNER = proxyAdminOwner || '0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253'
 
   // Fetch factories
   this.GemJoin = await hre.ethers.getContractFactory('GemJoin')
