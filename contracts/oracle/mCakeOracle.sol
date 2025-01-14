12d819ec0e106dc87fb2d351b12b67a465cc1a02
rick <jing.xin@lista.org>
2025-01-14 11:51:34 +0800

    add collateral mBTC mCake mwBETH testnet deploy scripts

---
+++ contracts/oracle/mCakeOracle.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./interfaces/AggregatorV3Interface.sol";
+import "./interfaces/IResilientOracle.sol";
+
+contract mCakeOracle is Initializable {
+  IResilientOracle public constant resilientOracle = IResilientOracle(0xf3afD82A4071f272F403dC176916141f44E6c750);
+  address public constant CAKE_TOKEN = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82; // cake token address
+
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize() external initializer {}
+
+  /**
+   * Returns the latest price
+   */
+  function peek() public view returns (bytes32, bool) {
+    uint256 price = resilientOracle.peek(CAKE_TOKEN);
+    return (bytes32(uint(price) * 1e10), true);
+  }
+}
---
+++ scripts/dev/new_collaterals/add_collateral_mcake.js
@@ -0,0 +1,98 @@
+const {ethers, upgrades} = require('hardhat')
+const hre = require('hardhat')
+// Global Variables
+let ray = '000000000000000000000000000', // 27 Decimals
+    rad = '000000000000000000000000000000000000000000000' // 45 Decimals
+
+async function main() {
+
+    const [deployer] = await ethers.getSigners()
+    const {symbol, tokenAddress, ilk, gemJoin, clipper, oracle} = {
+        "symbol": "mCake",
+        "tokenAddress": "0x70ad940d73415CDDAc47861e9691795AA7a119e1",
+        "ilk": "0x6d43616b65000000000000000000000000000000000000000000000000000000",
+        "gemJoin": "0x7A72B8294919eC9457049AAe2A35B9CdA161Bfa4",
+        "clipper": "0xB09aE793231fD7A1459059C941fC9225414cFC9B",
+        "oracle": "0x5c9558b51f2CC21d1419B9971801CF423c4d2C05",
+    }
+
+    // core parameters
+    const mat = '3000000000000000000000000000' // Liquidation Ratio
+    const line = '10000' + rad // Debt Ceiling
+    const dust = '15' + rad // Debt Floor
+    const hole = '5000000' + rad // Liquidation
+    const chop = '1100000000000000000' // Liquidation
+
+
+    let VAT = '0x33A34eAB3ee892D40420507B820347b1cA2201c4'
+    let DOG = '0xd57E7b53a1572d27A04d9c1De2c4D423f1926d0B'
+    let SPOT = '0x49bc2c4E5B035341b7d92Da4e6B267F7426F3038'
+    let INTERACTION = '0xB68443Ee3e828baD1526b3e0Bdf2Dfc6b1975ec4'
+    let AUCTION_PROXY
+
+    if (hre.network.name === 'bsc_testnet') {
+        VAT = '0x382589e4dE7A061fcb9716c203983d8FE847AE0b'
+        DOG = '0x3d2165EDf3Cc07992f54d9310FB800C81BC641F7'
+        SPOT = '0xa2882B6AC7cBA1b8784BF5D72F38CF0E6416263e'
+        INTERACTION = '0x70C4880A3f022b32810a4E9B9F26218Ec026f279'
+        if (!AUCTION_PROXY) {
+            // deploy AuctionProxy
+            const AuctionProxy = await hre.ethers.getContractFactory('AuctionProxy')
+            const auctionProxy = await AuctionProxy.deploy()
+            await auctionProxy.waitForDeployment()
+            AUCTION_PROXY = await auctionProxy.getAddress()
+            console.log('AuctionProxy deployed to:', AUCTION_PROXY)
+        }
+    }
+
+    console.log('symbol:        ' + symbol)
+    console.log('tokenAddress:  ' + tokenAddress)
+    console.log('ilk:           ' + ilk)
+
+    // configure the collateral
+    console.log('interaction...')
+    this.Interaction = await hre.ethers.getContractFactory('Interaction', {
+        unsafeAllow: ['external-library-linking'],
+        libraries: {
+            AuctionProxy: AUCTION_PROXY
+        },
+    })
+    const interaction = this.Interaction.attach(INTERACTION)
+    await interaction.setCollateralType(tokenAddress, gemJoin, ilk, clipper, mat)
+
+    console.log('vat...')
+    this.Vat = await hre.ethers.getContractFactory('Vat')
+    const vat = this.Vat.attach(VAT)
+    await vat.rely(gemJoin)
+    await vat.rely(clipper)
+    await vat['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('dust'), dust)
+    await vat['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('line'), line)
+
+    console.log('spot...')
+    this.Spot = await hre.ethers.getContractFactory('Spotter')
+    const spot = this.Spot.attach(SPOT)
+    await spot['file(bytes32,bytes32,address)'](ilk, ethers.encodeBytes32String('pip'), oracle)
+
+    console.log('Dog...')
+    this.Dog = await hre.ethers.getContractFactory('Dog')
+    const dog = this.Dog.attach(DOG)
+    await dog.rely(clipper)
+    await dog['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('hole'), hole)
+    await dog['file(bytes32,bytes32,uint256)'](ilk, ethers.encodeBytes32String('chop'), chop) // 10%
+    await dog['file(bytes32,bytes32,address)'](ilk, ethers.encodeBytes32String('clip'), clipper)
+
+
+    await interaction.setCollateralDuty(tokenAddress, '1000000002293273137447729405'); //apr 7.5%
+    console.log("set duty...");
+
+    await interaction.poke(tokenAddress, { gasLimit: 1000000 })
+    await interaction.drip(tokenAddress, { gasLimit: 1000000 })
+    console.log('Finished')
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
