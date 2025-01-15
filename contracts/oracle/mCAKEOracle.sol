bf9722e984c810311f330d57ad75d728092e92db
rick <jing.xin@lista.org>
2025-01-15 14:57:48 +0800

    fix: update mCake to mCAKE

--- contracts/oracle/mCAKEOracle.sol
+++ contracts/oracle/mCAKEOracle.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "./interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IResilientOracle.sol";
 
-contract mCakeOracle is Initializable {
+contract mCAKEOracle is Initializable {
   IResilientOracle public constant resilientOracle = IResilientOracle(0xf3afD82A4071f272F403dC176916141f44E6c750);
   address public constant CAKE_TOKEN = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82; // cake token address
 
