49fa08b6f28de429b1308ebbccdb95d600f0cef6
lawson-ccy <lawson.c@lista.org>
2025-05-06 15:05:50 +0800

    Changed all multi-sig addresses to timelock contract address

--- scripts/deploy/6_new_collateral_usd1.js
+++ scripts/deploy/6_new_collateral_usd1.js
@@ -10,7 +10,7 @@ async function main() {
 
   [deployer] = await ethers.getSigners()
   let NEW_OWNER = '0xAca0ed4651ddA1F43f00363643CFa5EBF8774b37'
-  let PROXY_ADMIN_OWNER = '0x08aE09467ff962aF105c23775B9Bc8EAa175D27F'
+  let PROXY_ADMIN_OWNER = '0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253'
 
   const symbol = 'USD1'
   let tokenAddress = '0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d' // USD1 token address on BSC Mainnet
