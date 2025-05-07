2f99ad1d035735c438ce230d6f1f00c859ca3f67
lawson-ccy <lawson.c@lista.org>
2025-05-07 22:10:02 +0800

    amended deployment scripts

--- scripts/prod/priceFeed/deploy_stable_asusdf_price_feed.js
+++ scripts/prod/priceFeed/deploy_stable_asusdf_price_feed.js
@@ -1,20 +1,20 @@
 const {ethers, upgrades} = require('hardhat')
 const hre = require('hardhat')
+const {deployImplementation, verifyImpContract} = require("../../upgrades/utils/upgrade_utils");
 
 const RESILIENT_ORACLE = "0xf3afD82A4071f272F403dC176916141f44E6c750"
 const ASUSDF_EARN = "0xdB57a53C428a9faFcbFefFB6dd80d0f427543695"
 
 async function main() {
-  const StableASUSDFPriceFeed = await hre.ethers.getContractFactory('StableAsUsdfPriceFeed');
-  const stableAsusdfPriceFeed = await upgrades.deployProxy(StableASUSDFPriceFeed, [RESILIENT_ORACLE, ASUSDF_EARN]);
-  await stableAsusdfPriceFeed.waitForDeployment();
+  console.log(`[Stable AsUSDF Price Feed] Deploying...`)
+  const factory = await ethers.getContractFactory('StableAsUsdfPriceFeed');
+  const contract = await factory.deploy(RESILIENT_ORACLE, ASUSDF_EARN);
+  await contract.deploymentTransaction().wait(6);
+  const address = await contract.getAddress();
+  console.log('[Stable AsUSDF Price Feed] Deployed to:', address);
 
-  console.log('StableAsusdfPriceFeed deployed to:', stableAsusdfPriceFeed.target);
-  // verify contract
-  await run("verify:verify", {
-    address: stableAsusdfPriceFeed.target,
-    constructorArguments: [],
-  });
+  console.log(`[Stable AsUSDF Price Feed] Verifying...`)
+  await hre.run("verify:verify", {address, constructorArguments: [RESILIENT_ORACLE, ASUSDF_EARN]});
 }
 
 main()
