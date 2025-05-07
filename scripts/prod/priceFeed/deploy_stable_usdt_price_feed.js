bd7727cc923e6071f9c1f95bead14ffc5f67ee4b
lawson-ccy <lawson.c@lista.org>
2025-05-07 22:10:02 +0800

    amended deployment scripts

--- scripts/prod/priceFeed/deploy_stable_usdt_price_feed.js
+++ scripts/prod/priceFeed/deploy_stable_usdt_price_feed.js
@@ -1,20 +1,18 @@
-const {ethers, upgrades} = require('hardhat')
-const hre = require('hardhat')
+const { verifyImpContract } = require('../../upgrades/utils/upgrade_utils')
+const {ethers} = require("hardhat");
 
 const RESILIENT_ORACLE = "0xf3afD82A4071f272F403dC176916141f44E6c750"
 
 async function main() {
-  const StableUSDTPriceFeed = await hre.ethers.getContractFactory('StableUsdtPriceFeed');
-  const stableUsdtPriceFeed = await upgrades.deployProxy(StableUSDTPriceFeed, [RESILIENT_ORACLE]);
-  await stableUsdtPriceFeed.waitForDeployment();
+  console.log(`[Stable USDT Price Feed] Deploying...`)
+  const factory = await ethers.getContractFactory('StableUsdtPriceFeed');
+  const contract = await factory.deploy(RESILIENT_ORACLE);
+  await contract.deploymentTransaction().wait(6);
+  const address = await contract.getAddress();
+  console.log('[Stable USDT Price Feed] Deployed to:', address);
 
-  console.log('StableUsdtPriceFeed deployed to:', stableUsdtPriceFeed.target);
-  // verify contract
-  await run("verify:verify", {
-    address: stableUsdtPriceFeed.target,
-    constructorArguments: [],
-  });
-
+  console.log(`[Stable USDT Price Feed] Verifying...`)
+  await hre.run("verify:verify", {address, constructorArguments: [RESILIENT_ORACLE]});
 }
 
 main()
