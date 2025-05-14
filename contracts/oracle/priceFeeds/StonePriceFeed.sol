c61aa91e4a8591c3aef928a916c4ae66f6b2a26e
lawson-ccy <lawson.c@lista.org>
2025-05-14 12:48:07 +0800

    Use API3Proxy interface directly

--- contracts/oracle/priceFeeds/StonePriceFeed.sol
+++ contracts/oracle/priceFeeds/StonePriceFeed.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "../interfaces/IResilientOracle.sol";
-import "../interfaces/AggregatorV3Interface.sol";
+import { IAPI3Proxy } from "../interfaces/OracleInterface.sol";
 import "../libraries/FullMath.sol";
 
 /**
@@ -12,14 +12,18 @@ import "../libraries/FullMath.sol";
 contract StonePriceFeed {
 
   IResilientOracle public resilientOracle;
-  AggregatorV3Interface public stoneEthPriceFeed;
+  IAPI3Proxy public stoneEthPriceFeed;
   address public constant ETH_TOKEN_ADDR = 0x2170Ed0880ac9A755fd29B2688956BD959F933F8;
 
-
+  /**
+    * @dev Constructor
+    * @param _resilientOracle The address of the Resilient Oracle contract
+    * @param _stoneEthPriceFeed The address of the Stone/ETH price feed contract
+    */
   constructor(address _resilientOracle, address _stoneEthPriceFeed) {
     require(_resilientOracle != address(0) && _stoneEthPriceFeed != address(0), "Zero address provided");
     resilientOracle = IResilientOracle(_resilientOracle);
-    stoneEthPriceFeed = AggregatorV3Interface(_stoneEthPriceFeed);
+    stoneEthPriceFeed = IAPI3Proxy(_stoneEthPriceFeed);
   }
 
   function decimals() external pure returns (uint8) {
@@ -74,21 +78,14 @@ contract StonePriceFeed {
     */
   function getPrice() private view returns (uint256 price) {
     // Stone/ETH in 8 DPs
-    (
-    /*uint80 roundID*/,
-      int stoneEthPrice,
-    /*uint startedAt*/,
-      uint updatedAt,
-    /*uint80 answeredInRound*/
-    ) = stoneEthPriceFeed.latestRoundData();
-
+    (int224 stoneEthPrice, uint32 updatedAt) = stoneEthPriceFeed.read();
     require(stoneEthPrice > 0, "stoneEthPriceFeed/price-not-valid");
-    require(block.timestamp - updatedAt < (24 * 3600 + 300), "stoneEthPriceFeed/timestamp-too-old");
+    require(block.timestamp - uint256(updatedAt) < (24 * 3600 + 300), "stoneEthPriceFeed/timestamp-too-old");
 
     // ETH/USD in 8 DPs
     uint256 ethPrice = resilientOracle.peek(ETH_TOKEN_ADDR);
 
-    return FullMath.mulDiv(uint256(stoneEthPrice), ethPrice, 1e8);
+    return FullMath.mulDiv(uint256(int256(stoneEthPrice)), ethPrice, 1e8);
   }
 
 }
