79768ab7efb99cca6285eb875bf859ee9538b5db
lawson-ccy <lawson.c@lista.org>
2025-05-20 16:50:48 +0800

    ! edited Stone/USD, sUSDe/USD price feed deployment scripts

--- scripts/prod/priceFeed/deploy_susde_price_feed.js
+++ scripts/prod/priceFeed/deploy_susde_price_feed.js
@@ -1,21 +1,20 @@
 const {ethers, upgrades} = require('hardhat')
 const hre = require('hardhat')
 
-// https://data.chain.link/feeds/bsc/mainnet/usde-usd
-let USDe_USD_PRICE_FEED = "0x10402B01cD2E6A9ed6DBe683CbC68f78Ff02f8FC"
+const RESILIENT_ORACLE = "0xf3afD82A4071f272F403dC176916141f44E6c750"
 // https://data.chain.link/feeds/bsc/mainnet/susde-usde-exchange-rate
-let sUSDe_USDe_PRICE_FEED = "0x1a269eA1b209DA2c12bDCDab22635C9e6C5028B2"
+const sUSDe_USDe_PRICE_FEED = "0x1a269eA1b209DA2c12bDCDab22635C9e6C5028B2"
 
 async function main() {
   console.log(`[sUSDe Price Feed] Deploying...`)
   const factory = await ethers.getContractFactory('sUSDePriceFeed');
-  const contract = await factory.deploy(sUSDe_USDe_PRICE_FEED, USDe_USD_PRICE_FEED);
+  const contract = await factory.deploy(RESILIENT_ORACLE, sUSDe_USDe_PRICE_FEED);
   await contract.deploymentTransaction().wait(6);
   const address = await contract.getAddress();
   console.log('[sUSDe Price Feed] Deployed to:', address);
 
   console.log(`[sUSDe Price Feed] Verifying...`)
-  await hre.run("verify:verify", {address, constructorArguments: [sUSDe_USDe_PRICE_FEED, USDe_USD_PRICE_FEED]});
+  await hre.run("verify:verify", {address, constructorArguments: [RESILIENT_ORACLE, sUSDe_USDe_PRICE_FEED]});
 }
 
 main()
