50a7388324eaf566706fef5fb6e886f45a3a71eb
rick <jing.xin@lista.org>
2025-07-14 15:48:20 +0800

    update deploy_cdpLiquidator scripts

--- scripts/deploy/deploy_cdpLiquidator.ts
+++ scripts/deploy/deploy_cdpLiquidator.ts
@@ -9,7 +9,7 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   const admin = deployer.address;
   const manager = deployer.address;
-  const bot = deployer.address;
+  const bot = "0x08E83A96F4dA5DecC0e6E9084dDe049A3E84ca04";
   const lender = "0x64d94e715B6c03A5D8ebc6B2144fcef278EC6aAa";
   const interaction = "0xB68443Ee3e828baD1526b3e0Bdf2Dfc6b1975ec4";
   const lisUSD = "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5";
