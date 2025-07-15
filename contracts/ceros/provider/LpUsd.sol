cf291217afbe92a401595af6758ab8c5bad2a768
lawson-ccy <lawson.c@lista.org>
2025-07-16 00:10:34 +0800

    + LP_USD: omni ERC721-ERC20 collateral for CDP

--- contracts/ceros/provider/LpUsd.sol
+++ contracts/ceros/provider/LpUsd.sol
@@ -10,10 +10,8 @@ import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
   */
 contract LpUsd is ERC20Upgradeable, Ownable2StepUpgradeable {
 
-  string public minter;
-
-  event MinterChanged(string oldMinter, string newMinter);
-
+  address public minter;
+  event MinterChanged(address oldMinter, address newMinter);
   modifier onlyMinter() {
     require(msg.sender == minter, "LpUsd/only-minter");
     _;
@@ -24,26 +22,45 @@ contract LpUsd is ERC20Upgradeable, Ownable2StepUpgradeable {
     _disableInitializers();
   }
 
-  function initialize(string _name, string _symbol, address _minter) external initializer {
+  /**
+    * @dev Initialize the contract with the name, symbol and minter address
+    * @param _name Name of the token
+    * @param _symbol Symbol of the token
+    * @param _minter Address of the minter
+    */
+  function initialize(string memory _name, string memory _symbol, address _minter) external initializer {
     require(_minter != address(0), "LpUsd/zero-address-minter");
     __Ownable2Step_init();
     __ERC20_init(_name, _symbol);
     minter = _minter;
   }
 
+  /**
+    * @dev Mint tokens to a specific account
+    * @param account Address of the account to mint tokens to
+    * @param amount Amount of tokens to mint
+    */
   function mint(address account, uint256 amount) external onlyMinter {
     _mint(account, amount);
   }
 
+  /**
+    * @dev Burn tokens from a specific account
+    * @param account Address of the account to burn tokens from
+    * @param amount Amount of tokens to burn
+    */
   function burn(address account, uint256 amount) external onlyMinter {
       _burn(account, amount);
   }
 
+  /**
+    * @dev Set a new minter address
+    * @param _minter Address of the new minter
+    */
   function setMinter(address _minter) external onlyOwner {
-    require(_minter != address(0), "LpUsd/zero-address-minter");
+    require(_minter != address(0) && minter != _minter, "LpUsd/invalid-minter");
     address oldMinter = minter;
     minter = _minter;
     emit MinterChanged(oldMinter, minter);
   }
-
 }
