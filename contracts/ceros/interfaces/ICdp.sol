72f311c2f84a3a7eff788327bed4fd5da1d79fc9
lawson-ccy <lawson.c@lista.org>
2025-07-24 21:40:03 +0800

    ! renamed ICdp interface variable names

--- contracts/ceros/interfaces/ICdp.sol
+++ contracts/ceros/interfaces/ICdp.sol
@@ -6,18 +6,18 @@ import "../../interfaces/GemJoinLike.sol";
 
 struct CollateralType {
   GemJoinLike gem;
-  bytes32 ilk;
+  bytes32 ilk; // ilk, bytes32, e.g. "ETH-A"
   uint32 live; //0 - inactive, 1 - started, 2 - stopped
-  address clip;
+  address clip; // auction contract address
 }
 
 interface VatLike {
   function ilks(bytes32) external view returns (
-    uint256 art, // Total Normalised Debt     [wad]
-    uint256 rate, // Accumulated Rates         [ray]
-    uint256 spot, // Price with Safety Margin  [ray]
-    uint256 line, // Debt Ceiling              [rad]
-    uint256 dust // Urn Debt Floor            [rad]
+    uint256 normalisedDebt, // Normalised Debt
+    uint256 interestRate, // Accumulated Rates, refreshed when called drip() at CDP
+    uint256 ltv, // loan to value - debt < collateral * ltv
+    uint256 maxLoan, // max. debt - user can only borrow at most this amount
+    uint256 minLoan // min. debt - user must borrow at least this amount
   );
 }
 
@@ -25,19 +25,20 @@ interface SpotLike {
   function ilks(bytes32) external view returns (PipLike, uint256);
 }
 
+// denominator when calculating interests
 uint256 constant RAY = 10 ** 27;
 
 interface ICdp {
   function deposit(
     address participant,
     address token,
-    uint256 dink
+    uint256 amount // amount of LP_USD
   ) external returns (uint256);
 
   function withdraw(
     address participant,
     address token,
-    uint256 dink
+    uint256 amount // amount of LP_USD
   ) external returns (uint256);
 
   function collaterals(address token) external view returns (GemJoinLike, bytes32, uint32, address);
