0af2337bf77bec07b3cf589822db55f93a360aa6
Lawson Cheng <lawson.c@binance.com>
2025-08-24 00:21:38 +0800

    ! fix: added unchecked to PcsV3LpNumbersHelper

--- contracts/ceros/provider/pancakeswapLpProvider/libraries/PcsV3LpNumbersHelper.sol
+++ contracts/ceros/provider/pancakeswapLpProvider/libraries/PcsV3LpNumbersHelper.sol
@@ -27,52 +27,54 @@ library PcsV3LpNumbersHelper {
   view
   returns (uint256 amount0, uint256 amount1)
   {
-    // Get fee, tickLower, tickUpper and liquidity
-    (
-      /* uint96 nonce */,
-      /* address operator */,
-      /* address token0 */,
-      /* address token1 */,
-      /* uint24 fee */,
-      int24 tickLower,
-      int24 tickUpper,
-      uint128 liquidity,
-      //// @note we didn't count the fees to make the amounts more conservative
-      /* uint256 feeGrowthInside0LastX128 */,
-      /* uint256 feeGrowthInside1LastX128 */,
-      /* uint128 tokensOwed0 */,
-      /* uint128 tokensOwed1 */
-    ) = INonfungiblePositionManager(nonFungiblePositionManager).positions(tokenId);
+    unchecked {
+      // Get fee, tickLower, tickUpper and liquidity
+      (
+        /* uint96 nonce */,
+        /* address operator */,
+        /* address token0 */,
+        /* address token1 */,
+        /* uint24 fee */,
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 liquidity,
+        //// @note we didn't count the fees to make the amounts more conservative
+        /* uint256 feeGrowthInside0LastX128 */,
+        /* uint256 feeGrowthInside1LastX128 */,
+        /* uint128 tokensOwed0 */,
+        /* uint128 tokensOwed1 */
+      ) = INonfungiblePositionManager(nonFungiblePositionManager).positions(tokenId);
 
-    // @note To prevent manipulation of the price,
-    //       we compute the fair sqrtPriceX96 instead of using slot0 from the pool.
-    //       (uint160 sqrtPriceX96,,,,,,) = IUniswapV3Pool(poolAddress).slot0();
+      // @note To prevent manipulation of the price,
+      //       we compute the fair sqrtPriceX96 instead of using slot0 from the pool.
+      //       (uint160 sqrtPriceX96,,,,,,) = IUniswapV3Pool(poolAddress).slot0();
 
-    // calculate fair sqrtPriceX96
-    uint160 sqrtPriceX96 = computeFairSqrtPriceX96(resilientOracle, token0, token1);
-    uint160 sqrtPriceX96Lower = TickMath.getSqrtRatioAtTick(tickLower);
-    uint160 sqrtPriceX96Upper = TickMath.getSqrtRatioAtTick(tickUpper);
-    (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
-      sqrtPriceX96, sqrtPriceX96Lower, sqrtPriceX96Upper, liquidity
-    );
-    // normalize amount0 and amount1 into 18 decimal places
-    uint8 decimals0 = IERC20Metadata(token0).decimals();
-    uint8 decimals1 = IERC20Metadata(token1).decimals();
+      // calculate fair sqrtPriceX96
+      uint160 sqrtPriceX96 = computeFairSqrtPriceX96(resilientOracle, token0, token1);
+      uint160 sqrtPriceX96Lower = TickMath.getSqrtRatioAtTick(tickLower);
+      uint160 sqrtPriceX96Upper = TickMath.getSqrtRatioAtTick(tickUpper);
+      (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
+        sqrtPriceX96, sqrtPriceX96Lower, sqrtPriceX96Upper, liquidity
+      );
+      // normalize amount0 and amount1 into 18 decimal places
+      uint8 decimals0 = IERC20Metadata(token0).decimals();
+      uint8 decimals1 = IERC20Metadata(token1).decimals();
 
-    if (decimals0 < 18) {
-      amount0 = amount0 * (10 ** (18 - decimals0));
-    } else if (decimals0 > 18) {
-      amount0 = amount0 / (10 ** (decimals0 - 18));
+      if (decimals0 < 18) {
+        amount0 = amount0 * (10 ** (18 - decimals0));
+      } else if (decimals0 > 18) {
+        amount0 = amount0 / (10 ** (decimals0 - 18));
+      }
+
+      if (decimals1 < 18) {
+        amount1 = amount1 * (10 ** (18 - decimals1));
+      } else if (decimals1 > 18) {
+        amount1 = amount1 / (10 ** (decimals1 - 18));
+      }
+      // verification of zero liquidity when user deposit
+      require(liquidity > 0, "PcsV3LpNumbersHelper: zero-liquidity");
+      require(amount0 > 0 || amount1 > 0, "PcsV3LpNumbersHelper: zero-token-amounts");
     }
-
-    if (decimals1 < 18) {
-      amount1 = amount1 * (10 ** (18 - decimals1));
-    } else if (decimals1 > 18) {
-      amount1 = amount1 / (10 ** (decimals1 - 18));
-    }
-    // verification of zero liquidity when user deposit
-    require(liquidity > 0, "PcsV3LpNumbersHelper: zero-liquidity");
-    require(amount0 > 0 || amount1 > 0, "PcsV3LpNumbersHelper: zero-token-amounts");
   }
 
   // @dev reference from https://github.com/sky-ecosystem/univ3-lp-oracle/blob/master/src/GUniLPOracle.sol
@@ -100,26 +102,28 @@ library PcsV3LpNumbersHelper {
   }
 
   function sqrt(uint256 _x) private pure returns (uint128) {
-    if (_x == 0) return 0;
-    else {
-      uint256 xx = _x;
-      uint256 r = 1;
-      if (xx >= 0x100000000000000000000000000000000) { xx >>= 128; r <<= 64; }
-      if (xx >= 0x10000000000000000) { xx >>= 64; r <<= 32; }
-      if (xx >= 0x100000000) { xx >>= 32; r <<= 16; }
-      if (xx >= 0x10000) { xx >>= 16; r <<= 8; }
-      if (xx >= 0x100) { xx >>= 8; r <<= 4; }
-      if (xx >= 0x10) { xx >>= 4; r <<= 2; }
-      if (xx >= 0x8) { r <<= 1; }
-      r = (r + _x / r) >> 1;
-      r = (r + _x / r) >> 1;
-      r = (r + _x / r) >> 1;
-      r = (r + _x / r) >> 1;
-      r = (r + _x / r) >> 1;
-      r = (r + _x / r) >> 1;
-      r = (r + _x / r) >> 1;
-      uint256 r1 = _x / r;
-      return uint128 (r < r1 ? r : r1);
+    unchecked {
+      if (_x == 0) return 0;
+      else {
+        uint256 xx = _x;
+        uint256 r = 1;
+        if (xx >= 0x100000000000000000000000000000000) { xx >>= 128; r <<= 64; }
+        if (xx >= 0x10000000000000000) { xx >>= 64; r <<= 32; }
+        if (xx >= 0x100000000) { xx >>= 32; r <<= 16; }
+        if (xx >= 0x10000) { xx >>= 16; r <<= 8; }
+        if (xx >= 0x100) { xx >>= 8; r <<= 4; }
+        if (xx >= 0x10) { xx >>= 4; r <<= 2; }
+        if (xx >= 0x8) { r <<= 1; }
+        r = (r + _x / r) >> 1;
+        r = (r + _x / r) >> 1;
+        r = (r + _x / r) >> 1;
+        r = (r + _x / r) >> 1;
+        r = (r + _x / r) >> 1;
+        r = (r + _x / r) >> 1;
+        r = (r + _x / r) >> 1;
+        uint256 r1 = _x / r;
+        return uint128 (r < r1 ? r : r1);
+      }
     }
   }
 
